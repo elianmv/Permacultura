0fe90ba89a5a1ae61fdda3ae9ea1aca42169ed66
elianmv <53497142+elianmv@users.noreply.github.com>
2022-08-10 21:26:27 -0300

    Add files via upload

---
+++ permacultura/frontend/src/pages/Home/Home.js
@@ -0,0 +1,11 @@
+import { Menu } from '../../components';
+import './Home.css';
+
+export function Home() {
+  return (
+    <>
+      <Menu />
+      <h3>Home Page</h3>
+    </>
+  );
+}
---
+++ permacultura/frontend/src/pages/Private/Private.js
@@ -0,0 +1,11 @@
+import { Menu } from '../../components';
+import './Private.css';
+
+export function Private() {
+  return (
+    <>
+      <Menu />
+      <h3>Private Page</h3>
+    </>
+  );
+}
