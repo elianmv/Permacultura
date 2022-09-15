78295e9d6c042d80fbc8765f64ff9f3d43f49e8c
Elian Vergara <elianvergara028@gmail.com>
2022-09-15 19:55:43 -0300

    retoques de registro

--- frontend/src/pages/Register/Register.js
+++ frontend/src/pages/Register/Register.js
@@ -1,5 +1,7 @@
 import { Menu } from '../../components';
 import './Register.css';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 
 
@@ -8,11 +10,67 @@ export function Register() {
   return (
     <>
       <Menu />
-      <h3>Register</h3>
- 
+      <h3>Registro de usuarios</h3>
+      <Form>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Nombre</Form.Label>
+        <Form.Control type="text" placeholder="Ingrese su Nombre" />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Apellido</Form.Label>
+        <Form.Control type="text" placeholder="Ingrese su Apellido" /> 
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Nombre de Usuario</Form.Label>
+        <Form.Control type="text" placeholder="Nombre de Usuario" />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Pais</Form.Label>
+        <Form.Control type="text" placeholder="" />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Ciudad</Form.Label>
+        <Form.Control type="text" placeholder="" />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Calle</Form.Label>
+        <Form.Control type="text" placeholder="" />
+      </Form.Group>
+      
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Numero</Form.Label>
+        <Form.Control type="text" placeholder="" />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email</Form.Label>
+        <Form.Control type="email" placeholder="Ingrese su Email" />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Cree una contraseña</Form.Label>
+        <Form.Control type="password1" placeholder="Contraseña" />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label> Repita su Contraseña</Form.Label>
+        <Form.Control type="password2" placeholder="Contraseña" />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicCheckbox">
+        <Form.Check type="checkbox" label="Check me out" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
     </>
   );
 }
  
-
 
